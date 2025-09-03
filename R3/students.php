785bb6576b6aa7d6dacfc100aa4d2010522f6ca0
mylinuxtests-lgtm <mylinuxtests@gmail.com>
2025-09-03 15:16:31 -0600

    Add files via upload

---
+++ R3/students.php
@@ -0,0 +1,103 @@
+<?php
+$servername = "localhost";
+$username   = "root";
+$password   = "0000";
+$dbname     = "students";
+$port       = "755";
+
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+$conn->set_charset("utf8mb4");
+
+if ($conn->connect_error) {
+    die("Error en la conexión: " . $conn->connect_error);
+}
+
+
+$sql = "
+
+
+SELECT
+  s.id_students   AS ID,
+  s.nombre        AS Nombre,
+  sex.descripcion AS Sexo,
+  s.edad          AS Edad,
+  DATE_FORMAT(s.nacimiento, '%d-%m-%Y') AS Fecha_Nacimiento,
+  p.pais          AS Pais,
+  s.telefono      AS Telefono,
+  s.correo        AS Correo,
+  s.domicilio     AS Domicilio,
+  s.foto          AS Foto,
+  s.lista         AS Lista,
+  s.excel         AS Excel
+FROM student s
+JOIN sexo sex ON s.id_sexo = sex.id_sexo
+JOIN paises p ON s.id_paises = p.id_paises
+";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<h2>Estudiantes Registrados</h2>";
+    echo "<a href='Formulario.html'>Volver al Formulario</a><br><br>";
+    
+    echo "<table border='1' cellpadding='5' style='border-collapse: collapse;'>";
+    echo "<tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Sexo</th>
+            <th>Edad</th>
+            <th>Fecha Nacimiento</th>
+            <th>Pais</th>
+            <th>Telefono</th>
+            <th>Correo</th>
+            <th>Domicilio</th>
+            <th>Foto</th>
+            <th>Lista</th>
+            <th>Excel</th>
+          </tr>";
+    
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>".$row['ID']."</td>";
+        echo "<td>".$row['Nombre']."</td>";
+        echo "<td>".$row['Sexo']."</td>";
+        echo "<td>".$row['Edad']."</td>";
+        echo "<td>".$row['Fecha_Nacimiento']."</td>";
+        echo "<td>".$row['Pais']."</td>";
+        echo "<td>".$row['Telefono']."</td>";
+        echo "<td>".$row['Correo']."</td>";
+        echo "<td>".$row['Domicilio']."</td>";
+        
+        
+        echo "<td><img src='./".$row['Foto']."' width='80' height='100' style='object-fit: cover;'></td>";
+        
+        
+        echo "<td><a href='./".$row['Lista']."' download>Mostrar lista</a></td>";
+        
+        
+        echo "<td>
+                <a href='./".$row['Excel']."'>
+                    <img src='./uploads/download.png' width='80' alt=''>
+                </a>
+              </td>";
+        
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "No se encontraron resultados.";
+    echo "<br><a href='Formulario.html'>Regresar al Formulario</a>";
+}
+
+$conn->close();
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <link rel="stylesheet" href="sstyle.css" media="screen" />
+    <title>Base de datos</title>
+</head>
+<body>
+</body>
+</html>
